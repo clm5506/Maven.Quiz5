a9b0beb365416b9b68f705a289e8f100344c896a
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-04-08 11:31:53 -0400

    check in 2

--- src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
@@ -1,37 +1,51 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author leon on 18/12/2018.
  */
-public class Group<_> {
+public class Group<_> implements GroupInterface<_>{
+
+    List<_> list;
+
+
     public Group() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        list = new ArrayList<>();
     }
 
     public Integer count() {
-        return null;
+        return list.size();
     }
 
     public void insert(_ value) {
+        list.add(value);
     }
 
     public Boolean has(_ value) {
-        return null;
+        return list.contains(value);
     }
 
     public _ fetch(int indexOfValue) {
-        return null;
+        return list.get(indexOfValue);
     }
 
     public void delete(_ value) {
+        list.remove(value);
     }
 
     public void clear() {
+        list.clear();
     }
 
     public Iterator<_> iterator() {
-        return null;
+        return list.iterator();
+    }
+
+    @Override
+    public String toString() {
+        return list.toString();
     }
 }
